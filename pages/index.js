e78fe0456717114d5038f4f97c4c2e1018f824ca
Tom Hartman <94thartman@gmail.com>
2021-09-21 23:45:37 -0400

    edited index page: * moved some sections to components * removed (most) tailwind classes and applied styles with traditional (ish) css * added extra styling to buttons and changed their destinations

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,20 @@
 import Head from "next/head";
 import Link from "next/link";
 import Footer from "../components/Footer";
+import LinkBoxes from "../components/LinkBoxes";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+		<div className="index">
 			<Head>
-				<title>Create Next App</title>
+				<title>Software Engineering Team 3</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
-				<h1 className="text-6xl font-bold">
-					Welcome to{" "}
-					<a className="nextjs text-red-600" href="https://nextjs.org">
-						Next.js!
-					</a>
-				</h1>
+			<main className="index__main">
+				<h2 className="title">
+					Welcome to <span className="nextjs">Group 3!</span>
+				</h2>
 
 				<p className="mt-3 text-2xl">
 					Get started by editing{" "}
@@ -24,48 +22,7 @@ export default function Home() {
 						pages/index.js
 					</code>
 				</p>
-
-				<div className="sm:w-full flex flex-wrap items-center justify-around max-w-4xl mt-6">
-					<a
-						href="https://nextjs.org/docs"
-						className="w-96 rounded-xl hover:text-blue-600 focus:text-blue-600 p-6 mt-6 text-left border"
-					>
-						<h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-						<p className="mt-4 text-xl">
-							Find in-depth information about Next.js features and API.
-						</p>
-					</a>
-
-					<a
-						href="https://nextjs.org/learn"
-						className="w-96 rounded-xl hover:text-blue-600 focus:text-blue-600 p-6 mt-6 text-left border"
-					>
-						<h3 className="text-2xl font-bold">Learn &rarr;</h3>
-						<p className="mt-4 text-xl">
-							Learn about Next.js in an interactive course with quizzes!
-						</p>
-					</a>
-
-					<a
-						href="https://github.com/vercel/next.js/tree/master/examples"
-						className="w-96 rounded-xl hover:text-blue-600 focus:text-blue-600 p-6 mt-6 text-left border"
-					>
-						<h3 className="text-2xl font-bold">Examples &rarr;</h3>
-						<p className="mt-4 text-xl">
-							Discover and deploy boilerplate example Next.js projects.
-						</p>
-					</a>
-
-					<a
-						href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className="w-96 rounded-xl hover:text-blue-600 focus:text-blue-600 p-6 mt-6 text-left border"
-					>
-						<h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-						<p className="mt-4 text-xl">
-							Instantly deploy your Next.js site to a public URL with Vercel.
-						</p>
-					</a>
-				</div>
+				<LinkBoxes></LinkBoxes>
 			</main>
 
 			<Footer></Footer>
