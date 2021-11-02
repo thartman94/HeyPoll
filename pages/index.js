97ef05982fa4357d1ed8a8a52714ef15de8b9be3
Brian Raley <raleyb12@students.rowan.edu>
2021-11-02 14:12:13 -0400

    home page buttons resized

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import Footer from "../components/Footer";
@@ -6,6 +6,8 @@ import Header from "../components/Header";
 import LinkBoxes from "../components/LinkBoxes";
 import HomePageButton from "../components/HomePageButton";
 import { JoinLobby, CreatePoll, Login } from "../functions/Functions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
 	return (
@@ -16,7 +18,6 @@ export default function Home() {
 			</Head>
 			<Header></Header>
 
-<<<<<<< HEAD
       <main className="index__main">
         <h1 className="title">
           Welcome to <span className="text-red-600 font-pacifico">HeyPoll</span>
@@ -39,35 +40,12 @@ export default function Home() {
             buttonClick={Login}
           ></HomePageButton>
         </div>
+		<div>
+			<button></button>
+		</div>
       </main>
 
       <Footer></Footer>
     </div>
   );
-=======
-			<main className="index__main">
-				<h1 className="title">
-					Welcome to <span className="font-pacifico text-red-600">HeyPoll</span>
-				</h1>
-				<br></br>
-				<div>
-					<HomePageButton
-						title={"Join a lobby"}
-						buttonClick={JoinLobby}
-					></HomePageButton>
-					<HomePageButton
-						title={"Create a poll (as guest)"}
-						buttonClick={CreatePoll}
-						path="/room"
-					></HomePageButton>
-					<HomePageButton
-						title={"Sign in / Sign up"}
-						buttonClick={Login}
-					></HomePageButton>
-				</div>
-			</main>
-			<Footer />
-		</div>
-	);
->>>>>>> ee560539ce2d083c42cec8a9f15f8103a7d2249b
 }
