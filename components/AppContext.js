b85c63fdfe277fa6c61c76ca1e94eaf4994a70c7
Tom Hartman <94thartman@gmail.com>
2021-11-18 20:02:03 -0500

    finished modal

--- components/AppContext.js
+++ components/AppContext.js
@@ -4,9 +4,12 @@ const AppContext = createContext({});
 
 export const AppProvider = ({ children }) => {
 	const [userRole, setRole] = useState("professor");
+	const [modalVisible, setModalVisibility] = useState(false);
 
 	return (
-		<AppContext.Provider value={{ userRole, setRole }}>
+		<AppContext.Provider
+			value={{ userRole, setRole, modalVisible, setModalVisibility }}
+		>
 			{children}
 		</AppContext.Provider>
 	);
--- components/Header.js
+++ components/Header.js
@@ -1,40 +1,11 @@
-import React from "react";
-import RoomCode from "../components/RoomCode";
+import React, { useContext } from "react";
+import Button from "./Button";
+import AppContext from "./AppContext";
+import Modal from "./Modal";
+import RoomCode from "./RoomCode";
 
 const Header = ({ title }) => {
-	const toggleModal = (e) => {
-		e.preventDefault();
-		const modal = e.target.closest(".header").querySelector(".info-modal");
-		const button = e.target.closest(".header").querySelector(".show-code");
-
-		if (modal.classList.contains("is-active")) {
-			modal.classList.remove("is-active");
-			button.innerHTML = "Show Room Code";
-			button.classList.remove("is-active");
-		} else {
-			modal.classList.add("is-active");
-			button.innerHTML = "Hide Room Code";
-			button.classList.add("is-active");
-		}
-	};
-
-	// const ShowInfoButton = () => {
-	// 	if (!(typeof window === "undefined")) {
-	// 		return window.location.href.includes("/lobbies") ? (
-	// 			<button className="show-code" onClick={toggleModal}>
-	// 				Show Room Code
-	// 			</button>
-	// 		) : null;
-	// 	} else {
-	// 		return null;
-	// 	}
-	// };
-
-	const RoomInfoModal = () => (
-		<div className="info-modal" onClick={toggleModal}>
-			{/* <RoomCode /> */}
-		</div>
-	);
+	const { modalVisible, setModalVisibility } = useContext(AppContext);
 
 	return (
 		<header className="header">
@@ -43,7 +14,17 @@ const Header = ({ title }) => {
 			</a>
 			<p className="page">{title}</p>
 			{/* <ShowInfoButton /> */}
-			<RoomInfoModal />
+			<Button
+				className={`header ${modalVisible && "is-active"}`}
+				onClick={() =>
+					setModalVisibility((prevModalVisible) => !prevModalVisible)
+				}
+			>
+				{modalVisible ? "Hide " : "Show "} Room Code
+			</Button>
+			<Modal>
+				<RoomCode />
+			</Modal>
 		</header>
 	);
 };
