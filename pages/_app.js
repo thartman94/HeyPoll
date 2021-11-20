70bb4a637770250ef40a3649b0b56f973e71c837
Tom Hartman <94thartman@gmail.com>
2021-11-19 20:28:08 -0500

    cleaning

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import "../styles/global.css";
 function MyApp({ Component, pageProps }) {
 	return (
 		<AppProvider>
-			<Component {...pageProps} />;
+			<Component {...pageProps} />
 		</AppProvider>
 	);
 }
