d48369723c063816725c10255b4220a7ebc41a35
flanne36 <flanne36@students.rowan.edu>
2021-11-23 14:51:59 -0500

    questions and polls now take an array for inputs

--- components/PollsSubMenu.js
+++ components/PollsSubMenu.js
@@ -13,8 +13,9 @@ import {
 
 export default function PollsSubMenu(){
 
+  // Adds questions to each poll
     function NumberList(props) {
-        const numbers = props.numbers;
+        const questions = props.questions;
         const listItems = questions.map((question) =>
           <MenuItem key={question.toString()}>
             {question}
@@ -23,46 +24,27 @@ export default function PollsSubMenu(){
         return (
           <MenuItem>{listItems}</MenuItem>
         );
-      }
-      const questions = ["Question 1", "Question 2", "Question 3", "Question 4"];
-
+    }
+// Adds polls to the side bar
+    function PollList(props) {
+      const polls = props.polls;
+      const listItems = polls.map((poll) =>
+        <SubMenu key={poll.toString()}
+        title={poll}>
+          <NumberList questions={questions} />
+        </SubMenu>
+        );
+        return (
+          listItems
+        );
+    }
     
+    const questions = ["Question 1", "Question 2", "Question 3", "Question 4"];
+    const polls = ["Poll 1","Poll 2", "Poll 3"]
 
     return (
         <div>
-        <SubMenu 
-        title="Poll 1"
-        >
-            <NumberList questions={questions} />
-        </SubMenu> 
-        <SubMenu
-        title="Poll 2"
-        >
-            <MenuItem>Question 1</MenuItem>
-            <MenuItem>Question 2</MenuItem>
-            <MenuItem>Question 3</MenuItem>
-            <MenuItem>Question 4</MenuItem>
-        </SubMenu>
-        <SubMenu
-        title="Poll 3"
-        >
-            <MenuItem>Question 1</MenuItem>
-            <MenuItem>Question 2</MenuItem>
-            <MenuItem>Question 3</MenuItem>
-            <MenuItem>Question 4</MenuItem>
-            <MenuItem>Question 1</MenuItem>
-            <MenuItem>Question 2</MenuItem>
-            <MenuItem>Question 3</MenuItem>
-            <MenuItem>Question 4</MenuItem>
-            <MenuItem>Question 5</MenuItem>
-            <MenuItem>Question 6</MenuItem>
-            <MenuItem>Question 1</MenuItem>
-            <MenuItem>Question 2</MenuItem>
-            <MenuItem>Question 3</MenuItem>
-            <MenuItem>Question 4</MenuItem>
-            <MenuItem>Question 5</MenuItem>
-            <MenuItem>Question 6</MenuItem>
-        </SubMenu>
+          <PollList polls={polls} />
         <div className="addPollSubmenu">
             <svg class="w-6 h-6 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
             <button className="addPollSubmenu__button">Add Poll</button>
