0804717ba4adde28d827a3f7a20255eceb0218a9
zmanwinner <zleesman@gmail.com>
2021-12-05 02:04:46 -0500

    put data in sidebar

--- components/SidebarItem.js
+++ components/SidebarItem.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { db } from "../firebase/clientApp";
+import { collection } from "@firebase/firestore";
 
-const SidebarItem = ({ title, questions }) => {
-	return (
-		<li>
-			<p>{title}</p>
-			<ul>
-				{questions.map((question, i) => (
-					<li key={i}>{question}</li>
-				))}
-			</ul>
-		</li>
-	);
+const SidebarItem = ({ title, id }) => {
+  const [questions, isQuestionsLoading, questionsError] = useCollectionData(
+    collection(db, "savedPolls", id, "pollQuestions"),
+    {
+      snapshotListenOptions: { includeMetadataChanges: true },
+    }
+  );
+  return (
+    <li>
+      <p>{title}</p>
+      <ul>
+        {!isQuestionsLoading &&
+          questions.map((question, i) => <li key={i}>{question.question}</li>)}
+      </ul>
+    </li>
+  );
 };
 
 export default SidebarItem;
